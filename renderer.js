7cdcf2a875f39cff4af6acc29237dbaa59738fa4
VU Manh Tu <glmanhtu@gmail.com>
2024-02-21 13:04:18 +0100

    Update

--- renderer.js
+++ renderer.js
@@ -36,16 +36,21 @@ ipcRenderer.on('selected-files', (event, files) => {
                 setActiveComponent(fullImage);
             })
             } else {
-            setActiveComponent(thumbnail.ref);
+                setActiveComponent(thumbnail.ref);
             }
         });
     });
 });
 
+repeatActionOnHold('i', () => zoomIn(0.01));
+repeatActionOnHold('o', () => zoomOut(0.01));
+repeatActionOnHold('r', () => rotateRight(1));
+repeatActionOnHold('l', () => rotateLeft(1));
+
 ipcRenderer.on('resized', (event, size) => {
     var height = size[1];
-    document.getElementById('thumbnail-column').setAttribute('style', `height: ${height - 100}px`)
-    document.getElementById('board-column').setAttribute('style', `height: ${height - 100}px`)
+    document.getElementById('thumbnail-column').style.height = `${height - 100}px`;
+    document.getElementById('board-column').style.height = `${height - 100}px`;
 });
 
 function setActiveComponent(comp) {
@@ -64,44 +69,44 @@ function openDialog() {
     ipcRenderer.send('open-file-dialog');
 }
 
-function rotateLeft() {
+function rotateLeft(step=5) {
     const image = getActiveComponent()
     if (image) {
-    const rotation = parseInt(image.dataset.rotation || 0) - 5;
-    const scale = parseFloat(image.dataset.scale || 1);
-    image.style.transform = `scale(${scale}) rotate(${rotation}deg)`;
-    image.dataset.rotation = rotation;
+        const rotation = parseInt(image.dataset.rotation || 0) - step;
+        const scale = parseFloat(image.dataset.scale || 1);
+        image.style.transform = `scale(${scale}) rotate(${rotation}deg)`;
+        image.dataset.rotation = rotation;
     }
 }
 
-function rotateRight() {
+function rotateRight(step=5) {
     const image = getActiveComponent()
     if (image) {
-    const rotation = parseInt(image.dataset.rotation || 0) + 5;
-    const scale = parseFloat(image.dataset.scale || 1);
-    image.style.transform = `scale(${scale}) rotate(${rotation}deg)`;
-    image.dataset.rotation = rotation;
+        const rotation = parseInt(image.dataset.rotation || 0) + step;
+        const scale = parseFloat(image.dataset.scale || 1);
+        image.style.transform = `scale(${scale}) rotate(${rotation}deg)`;
+        image.dataset.rotation = rotation;
     }
 }
 
-function zoomIn() {
+function zoomIn(step=0.1) {
     const image = getActiveComponent()
     if (image) {
-    const rotation = parseInt(image.dataset.rotation || 0);
-    const scale = parseFloat(image.dataset.scale || 1) + 0.1;
-    image.style.transform = `scale(${scale}) rotate(${rotation}deg)`;
+        const rotation = parseInt(image.dataset.rotation || 0);
+        const scale = parseFloat(image.dataset.scale || 1) + step;
+        image.style.transform = `scale(${scale}) rotate(${rotation}deg)`;
 
-    image.dataset.scale = scale;
+        image.dataset.scale = scale;
     }
 }
 
-function zoomOut() {
+function zoomOut(step=0.1) {
     const image = getActiveComponent()
     if (image) {
-    const rotation = parseInt(image.dataset.rotation || 0);
-    const scale = parseFloat(image.dataset.scale || 1) - 0.1;
-    image.style.transform = `scale(${scale}) rotate(${rotation}deg)`;
-    image.dataset.scale = scale;
+        const rotation = parseInt(image.dataset.rotation || 0);
+        const scale = parseFloat(image.dataset.scale || 1) - step;
+        image.style.transform = `scale(${scale}) rotate(${rotation}deg)`;
+        image.dataset.scale = scale;
     }
 }
 
@@ -110,34 +115,45 @@ function dragElement(elmnt) {
     elmnt.onmousedown = dragMouseDown;
 
     function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
     }
 
     function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    elmnt.style.cursor = "move";
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        elmnt.style.cursor = "move";
     }
 
     function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-    elmnt.style.cursor = "unset";
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+        elmnt.style.cursor = "unset";
     }
+}
+
+
+function repeatActionOnHold(key, actionFunction) {
+    // Event listener for keydown event
+    document.addEventListener('keydown', (event) => {
+        if (event.key === key) {
+            actionFunction()
+        }
+    });
+
 }
