d7f2184964d9043541b11d5ac357e85723b5d686
VU Manh Tu <glmanhtu@gmail.com>
2024-03-17 22:42:48 +0100

    Init angular project with electron and bootstrap

---
+++ app/main.ts
@@ -0,0 +1,78 @@
+import {app, BrowserWindow} from 'electron';
+import * as path from 'path';
+import * as fs from 'fs';
+
+let win: BrowserWindow | null = null;
+const args = process.argv.slice(1),
+  serve = args.some(val => val === '--serve');
+
+function createWindow(): BrowserWindow {
+
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1350,
+    height: 850,
+    minWidth: 1350,
+    minHeight: 850,
+    webPreferences: {
+      nodeIntegration: true,
+      allowRunningInsecureContent: (serve),
+      contextIsolation: false,
+      webSecurity: false
+    },
+  });
+
+  if (serve) {
+    const debug = require('electron-debug');
+    debug();
+
+    require('electron-reloader')(module);
+    win.loadURL('http://localhost:4200');
+  } else {
+    // Path when running electron executable
+    let pathIndex = './index.html';
+
+    if (fs.existsSync(path.join(__dirname, '../dist/index.html'))) {
+       // Path when running electron in local folder
+      pathIndex = '../dist/index.html';
+    }
+
+    const url = new URL(path.join('file:', __dirname, pathIndex));
+    win.loadURL(url.href);
+  }
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store window
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+
+  return win;
+}
+
+try {
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
+  app.on('ready', () => setTimeout(createWindow, 400));
+
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    app.quit();
+  });
+
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow();
+    }
+  });
+
+} catch (e) {
+  // Catch Error
+  // throw e;
+}
